dbc2a5f9eb7ac113781e20f23d15a9dedee4d33a
Mace Akkari <mace.akkari@relyfe.com>
2022-05-22 23:57:55 +0100

    add API key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+
+// api key --->  http://www.omdbapi.com/?i=tt3896198&apikey=6124488b
+
+const API_URL =  'http://www.omdbapi.com?apikey=6124488b';
 
 const App = () => {
+  const searchMovie = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data)
+  }
+
+
   return (
     <h1>App</h1>
   )
